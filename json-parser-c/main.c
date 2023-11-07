d29ce2bd10a3d747a9f2294f77a3bb925a52f790
Naman Agarwal <namanddev7@gmail.com>
2023-11-08 03:34:06 +0530

    prune spaces & \n

--- json-parser-c/main.c
+++ json-parser-c/main.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() {
+
+int pruneSpaces(char *json, int start, int end, char enclosing_block) {
+  int i = start;
+
+  while (i <= end && enclosing_block != '"' && (*(json + i) == ' ' || *(json + i) == '\n')) {
+    i++;
+  }
+  return i;
+}
+
+int reversePruneSpaces(char *json, int start, int end, char enclosing_block) {
+  int i = start;
+
+  while (i >= end && enclosing_block != '"' && (*(json + i) == ' ' || *(json + i) == '\n')) {
+    i--;
+  }
+  return i;
+}
+
+int main () {
   int DEBUG = 1;
 
-  char json[] = "{\"a\":[9.0]}";
+  char json[] = "{\"a\": [ 9 .0 ]}    ";
 
   int len = 0, i = 0;
 
@@ -20,15 +39,29 @@ int main() {
   int error = 0;
   int is_frist_curl = 1;
 
-  int decimal_point_found = 0;
+  int decimal_point_found = 0, found_digit = 0;
   char *enclosing_block = malloc((len / 2) * sizeof(char));
 
   *enclosing_block = '\0';
 
-  while (s <= e) {
-    if (s > e) break;
-    char ch1 = *(json + s);
+  e = reversePruneSpaces(json, len - 1, 0, *enclosing_block);
 
+  if (s > e) {
+    error = 1;
+  }
+
+  while (s <= e) {
+    int idx = pruneSpaces(json, s, e, *enclosing_block);
+
+    if (s != idx) {
+      found_digit = 0;
+      decimal_point_found = 0;
+    }
+    s = idx;
+
+    if (s > e) break;
+
+    char ch1 = *(json + s);
 
     if (DEBUG) {
       printf("ch: %c\nenclosing-block: %c\nch-next: %c\n\n", ch1, *enclosing_block, *(json + s + 1));
@@ -59,21 +92,29 @@ int main() {
       else if (*enclosing_block == '"') {
         if (ch1 == '"') {
           enclosing_block--;
-          if (*enclosing_block == '{' && *(json + s + 1) != ':') {
+          int peek = pruneSpaces(json, s + 1, e, *enclosing_block);
+          if (peek > e) {
             error = 1;
             break;
-          } else if (*enclosing_block == ':' && *(json + s + 1) != ',') {
+          }
+          
+          int next_ch = *(json + peek);
+
+          if (*enclosing_block == '{' && next_ch != ':') {
             error = 1;
             break;
-          } else if (*enclosing_block == '[' && (*(json + s + 1) != ',' || *(json + s + 1) != ']')) {
+          } else if (*enclosing_block == ':' && next_ch != ',') {
+            error = 1;
+            break;
+          } else if (*enclosing_block == '[' && (next_ch != ',' || next_ch != ']')) {
             error = 1;
             break;
           }
           
           if (*enclosing_block == '{') {
-            s++;
-            *(++enclosing_block) = *(json + s);
-          } else if (*enclosing_block == '[' && *(json + s + 1) == ',') s++;
+            s=peek;
+            *(++enclosing_block) = next_ch;
+          } else if (*enclosing_block == '[' && next_ch == ',') s=peek;
         }
       }
 
@@ -82,6 +123,7 @@ int main() {
           enclosing_block--;
           s++;
           decimal_point_found = 0;
+          found_digit = 0;
         } else if (ch1 == '}' && *(enclosing_block - 1) == '{') {
           enclosing_block--;
           s--;
@@ -93,7 +135,7 @@ int main() {
             error = 1;
             break;
           }
-          else if (*(json + s - 1) >= '0' && *(json + s - 1) <= '9') {
+          else if (found_digit) {
             decimal_point_found = 1;
           } else {
             error = 1;
@@ -103,6 +145,8 @@ int main() {
         } else if (ch1 < '0' || ch1 > '9') {
           error = 1;
           break;
+        } else {
+          found_digit = 1;
         }
       }
 
@@ -114,22 +158,23 @@ int main() {
           //   break;
           // }
           decimal_point_found = 0;
+          found_digit = 0;
           // s++;
         } else if (ch1 == ',') {
-          if (*(json + s - 1) == '[') {
+          if (!found_digit && *enclosing_block == '[') {
             error = 1;
             break;
           } else {
             decimal_point_found = 0;
+            found_digit = 0;
           }
         } else if (ch1 == '"') {
-          // only string array support
           *(++enclosing_block) = ch1;
         } else if (ch1 == '.') {
           if (decimal_point_found) {
             error = 1;
             break;
-          } else if (*(json + s - 1) >= '0' && *(json + s - 1) <= '9') {
+          } else if (found_digit) {
             decimal_point_found = 1;
           } else {
             error = 1;
@@ -138,6 +183,8 @@ int main() {
         } else if (ch1 < '0' || ch1 > '9') {
           error = 1;
           break;
+        } else {
+          found_digit = 1;
         }
       } else {
         error = 1;
